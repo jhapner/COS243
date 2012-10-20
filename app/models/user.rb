a01a42edb0fb655f9a550f90e34cf04453af9d83
Josh Hapner <joshua_hapner1@taylor.edu>
2012-10-20 17:00:04 -0400

    6.2.2 model validates name and email to be present

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,9 @@
 #  updated_at :datetime         not null
 #
 
-class User < ActiveRecord::Base
-  attr_accessible :email, :name
+class User < ActiveRecord::Base 
+  attr_accessible :name, :email
+
+  validates :name, presence: true
+  validates :email, presence: true
 end
