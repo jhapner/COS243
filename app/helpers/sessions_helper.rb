83e3bc13febea5e1ee3558acbae8e4f67443aced
Josh Hapner <joshua_hapner1@taylor.edu>
2012-10-26 00:09:35 -0400

    8.2.4 links change after signin

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -5,11 +5,15 @@ module SessionsHelper
     self.current_user = user
   end
 
+  def signed_in?
+    !current_user.nil?
+  end
+
   def current_user=(user)
     @current_user = user
   end
+
   def current_user
     @current_user ||= User.find_by_remember_token(cookies[:remember_token])
   end
 end
-
