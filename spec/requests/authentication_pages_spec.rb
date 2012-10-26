842a6adec8a115ffa4b715e5905ae8b6fcf151f9
Josh Hapner <joshua_hapner1@taylor.edu>
2012-10-26 00:20:39 -0400

    8.2.6 signing out

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -24,7 +24,8 @@ describe "Authentication" do
         it { should_not have_selector('div.alert.alert-error') }
       end   
    	end
-describe "with valid information" do
+    
+	describe "with valid information" do
       let(:user) { FactoryGirl.create(:user) }
       before do
         fill_in "Email",    with: user.email
@@ -36,6 +37,10 @@ describe "with valid information" do
       it { should have_link('Profile', href: user_path(user)) }
       it { should have_link('Sign out', href: signout_path) }
       it { should_not have_link('Sign in', href: signin_path) }
+      describe "followed by signout" do
+        before { click_link "Sign out" }
+        it { should have_link('Sign in') }
+      end
     end
   end
 end
