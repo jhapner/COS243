c4a83e552982d6646d3d62a0837f5040473be155
Josh Hapner <joshua_hapner1@taylor.edu>
2012-11-06 21:55:52 -0500

    10.2.1 user show page with to display microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,8 +3,10 @@ class UsersController < ApplicationController
   before_filter :correct_user,       only: [:edit, :update]
   before_filter :admin_user,         only: :destroy
   before_filter :non_signed_in_user, only: [:new, :create]
+  
   def show
     @user = User.find(params[:id])
+	@microposts = @user.microposts.paginate(page: params[:page])
   end
 
   def new
